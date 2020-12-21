8a25b9a3f225f1e05a241f7f4254d37f356a911e
Claudia Hernandez <claudiahdez13@gmail.com>
2020-12-21 00:17:47 -0800

    initial html and READme push

---
+++ script.js
@@ -0,0 +1,31 @@
+HTML Page
+- dashboard heading
+- container
+-- row
+
+--- left column
+---- city search input and button
+---- city search history
+
+--- right column
+---- Current Weather Conditions
+----- City Name
+----- Current Date
+----- Current Weather Icon
+----- Temperature
+----- humidity
+----- Wind Speed
+----- UV Index
+
+--- 5-Day Forecast:
+----- cards for the next 5 days
+---- Each card:
+----- date
+----- weather icon
+----- temp
+----- humidity
+
+
+Events:
+- User clicks on a search history item
+- User clicks on submit search button

02533a9ca726d5e597ec16ff13c62eeb8abaf63f
Claudia Hernandez <claudiahdez13@gmail.com>
2020-12-21 21:21:19 -0800

    added the five day forecast and edited html

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# weather-dashboard
-This is the template for a weather dashboard that displays current weather for multiple cities. This template was created using JavaScirpt, Bootstraps and third-party API, OpenWeather API. OpenWeater API is used to retrive weather data for cities. 
+
--- script.js
+++ script.js
@@ -25,65 +25,138 @@ $(function () {
       $("#wind").text(data.wind.speed + " mph");
 
       $("#humidity").text(data.main.humidity + " %");
-      $("#uvindex").text(data.main.uvIndex + " %");
+      /*this is to get the UV Index */
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
 
-      //--- right column
-      //---- Current Weather Conditions
-      //----- City Name
-      //----- Current Date
-      //----- Current Weather Icon
-      //----- Temperature
-      //----- humidity
-      //----- Wind Speed
-      //----- UV Index
+      var queryUrl1 =
+        "https://api.openweathermap.org/data/2.5/uvi?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=" +
+        apiKey;
+      $.ajax({
+        url: queryUrl1,
+        method: "GET",
+      }).then(function (uvData) {
+        console.log(uvData);
 
-      //--- 5-Day Forecast:
-      //----- cards for the next 5 days
-      //---- Each card:
-      //----- date
-      //----- weather icon
-      //----- temp
-      //----- humidity
-
-      //Events:
-      //- User clicks on a search history item
-      //- User clicks on submit search button
+        $("#uvIndex").text(uvData.value + " ");
+      });
     });
-  });
-});
-
-$("#search-form").on("submit", function (event) {
-    // prevent the default form behavior
-    event.preventDefault();
-    // store value of form input in a variable named search. remove any leading
-    // or trailing white space using the trim method
-    var search = $("#search-input").val().trim();
-    // do nothing if search has no characters (empty string)
-    if (search === "") {
-      return;
-    }
-   
     var queryUrl =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      search +
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
       "&units=imperial&appid=" +
       apiKey;
-    // send ajax request for current weather to OpenWeatherAPI
+
     $.ajax({
       url: queryUrl,
       method: "GET",
     }).then(function (data) {
-      // log the data from the api to the console
       console.log(data);
-      // display the city name, temperature, wind, and humidity in the elements
-      // found in the html.
-      $("#city-name").text(data.name + " Weather");
-      $("#temp").text(data.main.temp + "°");
-      $("#wind").text(data.wind.speed + "mph");
-      $("#humidity").text(data.main.humidity + "%");
-      $("#uvIndex").text(data.main.UVindex + "%");
-      // Clear the search input
-      $("#search-input").val("");
+      // log the data from the api to the console
+
+      $("#forecast-temp1").text(data.list[0].main.temp + " °");
+
+      $("#forecast-humidity1").text(data.list[0].main.humidity + " %");
+
+      $("#forecast-temp2").text(data.list[8].main.temp + " °");
+
+      $("#forecast-humidity2").text(data.list[8].main.humidity + " %");
+
+      $("#forecast-temp3").text(data.list[16].main.temp + " °");
+
+      $("#forecast-humidity3").text(data.list[16].main.humidity + " %");
+
+      $("#forecast-temp4").text(data.list[24].main.temp + " °");
+
+      $("#forecast-humidity4").text(data.list[24].main.humidity + " %");
+
+      $("#forecast-temp5").text(data.list[32].main.temp + " °");
+
+      $("#forecast-humidity5").text(data.list[32].main.humidity + " %");
     });
   });
+
+  /* code below is for the search input*/
+  $("#search-form").on("submit", function (event) {
+    // prevent the default form behavior
+    event.preventDefault();
+    // store value of form input in a variable named search. remove any leading
+    // or trailing white space using the trim method
+    var city = $("#search-input").val().trim();
+    // do nothing if search has no characters (empty string)
+    if (city === "") {
+      return;
+    }
+
+    var queryUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=imperial&appid=" +
+      apiKey;
+
+    $.ajax({
+      url: queryUrl,
+      method: "GET",
+    }).then(function (data) {
+      console.log(data);
+      // log the data from the api to the console
+      $("#city-name").text(data.name + " Weather");
+
+      $("#temp").text(data.main.temp + " °");
+
+      $("#wind").text(data.wind.speed + " mph");
+
+      $("#humidity").text(data.main.humidity + " %");
+
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      var queryUrl1 =
+        "https://api.openweathermap.org/data/2.5/uvi?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=" +
+        apiKey;
+      $.ajax({
+        url: queryUrl1,
+        method: "GET",
+      }).then(function (uvData) {
+        console.log(uvData);
+
+        $("#uvIndex").text(uvData.value + " ");
+      });
+    });
+    var queryUrl =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
+      "&units=imperial&appid=" +
+      apiKey;
+
+    $.ajax({
+      url: queryUrl,
+      method: "GET",
+    }).then(function (data) {
+      console.log(data);
+      // log the data from the api to the console
+      $("#forecast-temp1").text(data.list[0].main.temp + " °");
+
+      $("#temp").text(data.main.temp + " °");
+
+      $("#wind").text(data.wind.speed + " mph");
+
+      $("#humidity").text(data.main.humidity + " %");
+      $("#uvindex").text(data.main.coord.lon + " ");
+    });
+  });
+}) 
+  $("#search-form").on("click", function () {
+  var searchId = $(this).attr("id");
+  var id = searchId.replace("search-form", "");
+  var task = $("#" + id).val();
+  localStorage.setItem(id, task);
 });
+
